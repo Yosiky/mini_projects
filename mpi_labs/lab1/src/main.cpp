e608bf9b4d6b3b916df150e359b3bb4b103df855
eestelle <yosiky@list.ru>
2023-10-09 15:47:29 +0300

    fix: change function on C-style

--- mpi_labs/lab1/src/main.cpp
+++ mpi_labs/lab1/src/main.cpp
@@ -1,5 +1,6 @@
 #include <mpi.h>
-#include <iostream>
+/* #include <iostream> */
+#include <stdio.h>
 
 int main(int argc, char **argv) {
 
@@ -13,13 +14,16 @@ int main(int argc, char **argv) {
     if (proc_rank == 0) {
         MPI_Status status;
         int get_rank;
-        std::cout << "Hello, it's main proccess" << std::endl;
-        for (int i = 0; i < proc_size; ++i) {
+        printf("Hello, it's main proccess\n");
+        /* std::cout << "Hello, it's main proccess" << std::endl; */
+        for (int i = 1; i < proc_size; ++i) {
             MPI_Recv(&get_rank, 1, MPI_INT, MPI_ANY_SOURCE,
                 MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            std::cout << "Hello, it's " << get_rank << " proccess" << std::endl;
+            printf("Hello, it's %d main proccess\n", get_rank);
+            /* std::cout << "Hello, it's " << get_rank << " proccess" << std::endl; */
         }
-        std::cout << "Bye, it's main proccess" << std::endl;
+        printf("Bye, it's main proccess\n");
+        /* std::cout << "Bye, it's main proccess" << std::endl; */
     }
     else {
         MPI_Send(&proc_rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
